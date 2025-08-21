4cebe8b2b0cbaf11ca52e70011a3025d0d5e251e
akshay-ken <akshaybest3120@gmail.com>
2025-08-21 18:41:42 +0530

    style: style div background with light zinc color and padding

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -2,9 +2,9 @@ import Switch from "./ToggleSwitch";
 
 export function HeaderSection() {
   return (
-    <header className="flex flex-row items-baseline-last gap-x-2 md:gap-x-8 px-1 py-2 md:p-4 bg-red-400">
+    <header className="flex flex-row items-baseline-last gap-x-2 md:gap-x-8 px-1 py-2 md:p-4 ">
       <h1 className="font-zcool text-2xl md:text-7xl">Akshay Nalawade</h1>
-      <div className="text-[8px]/2 md:text-xl font-web font-semibold italic">
+      <div className="text-[8px]/2 md:text-xl bg-zinc-300 px-1 md:px-3 rounded-xs font-web font-semibold italic">
         <p>College Dropout</p>
         <p>Self-Taught Frontend web developer</p>
       </div>
