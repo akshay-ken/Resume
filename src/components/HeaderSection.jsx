8692a43ad0e3966d53ec81bf7fb02a157c8c35b1
akshay-ken <akshaybest3120@gmail.com>
2025-08-21 17:51:10 +0530

    style: style div tag with text classes

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,8 +1,8 @@
 export function HeaderSection() {
   return (
     <header className="flex flex-row">
-      <h1 className="font-zcool text-2xl md:text-7xl">Akshay Nalawade</h1>
-      <div>
+      <h1 className="font-zcool text-4xl md:text-7xl">Akshay Nalawade</h1>
+      <div className="text-xs/3 tracking-tighter md:text-xl font-zcool font-normal">
         <p>College Dropout</p>
         <p>Self-Taught Frontend web developer</p>
       </div>
