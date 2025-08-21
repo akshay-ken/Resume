78d2ce7eea67cf85a6ef1dbcc3a46274f4d54c33
akshay-ken <akshaybest3120@gmail.com>
2025-08-21 17:59:54 +0530

    style: add padding to header section

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,6 +1,6 @@
 export function HeaderSection() {
   return (
-    <header className="flex flex-row">
+    <header className="flex flex-row px-1 py-2 md:p-4 bg-red-400">
       <h1 className="font-zcool text-3xl md:text-7xl">Akshay Nalawade</h1>
       <div className="text-[8px]/2 md:text-xl font-web font-semibold italic">
         <p>College Dropout</p>
