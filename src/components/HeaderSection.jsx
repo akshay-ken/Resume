2f40592c1662729785845c3a666837f0706a2de8
akshay-ken <akshaybest3120@gmail.com>
2025-08-21 18:02:24 +0530

    style: align items to baseline with gap x

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,6 +1,6 @@
 export function HeaderSection() {
   return (
-    <header className="flex flex-row px-1 py-2 md:p-4 bg-red-400">
+    <header className="flex flex-row items-baseline-last gap-x-2 md:gap-x-8 px-1 py-2 md:p-4 bg-red-400">
       <h1 className="font-zcool text-3xl md:text-7xl">Akshay Nalawade</h1>
       <div className="text-[8px]/2 md:text-xl font-web font-semibold italic">
         <p>College Dropout</p>
