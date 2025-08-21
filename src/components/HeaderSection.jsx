7881b104590e3ba97c046678c6c3cb9e4af42782
akshay-ken <akshaybest3120@gmail.com>
2025-08-21 22:30:08 +0530

    style: add dark variant classes to header section

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -2,11 +2,11 @@ import Switch from "./ToggleSwitch";
 
 export function HeaderSection({ toggleDarkMode, checkValue }) {
   return (
-    <header className="flex flex-row items-baseline-last gap-x-2 md:gap-x-8 px-1 py-2 md:p-4 border-b-4 border-zinc-900">
-      <h1 className="font-zcool md:text-shadow-[5px_4px_2px] text-shadow-[2px_2px_1px] text-shadow-zinc-400 text-2xl md:text-6xl">
+    <header className="flex flex-row items-baseline-last gap-x-2 md:gap-x-8 px-1 py-2 md:p-4 border-b-4 border-zinc-900 dark:border-zinc-400">
+      <h1 className="font-zcool md:text-shadow-[5px_4px_2px] dark:text-zinc-300 text-shadow-[2px_2px_1px] text-shadow-zinc-400 text-2xl md:text-6xl">
         Akshay Nalawade
       </h1>
-      <div className="text-[8px]/2 md:text-xl bg-zinc-300 dark:bg-zinc-500 px-1 md:px-3 rounded-xs font-web font-semibold italic">
+      <div className="text-[8px]/2 md:text-xl bg-zinc-300 dark:bg-zinc-500 dark:text-zinc-100 px-1 md:px-3 rounded-xs font-web font-semibold italic">
         <p>College Dropout</p>
         <p>Self-Taught Frontend web developer</p>
       </div>
