9ca7fd81c64c7acc7b83504f18d4c6b0f3c5555e
akshay-ken <akshaybest3120@gmail.com>
2025-08-21 18:06:06 +0530

    feat: used toogle switch in hdearsection component

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,3 +1,5 @@
+import Switch from "./ToggleSwitch";
+
 export function HeaderSection() {
   return (
     <header className="flex flex-row items-baseline-last gap-x-2 md:gap-x-8 px-1 py-2 md:p-4 bg-red-400">
@@ -6,6 +8,7 @@ export function HeaderSection() {
         <p>College Dropout</p>
         <p>Self-Taught Frontend web developer</p>
       </div>
+      <Switch />
     </header>
   );
 }
