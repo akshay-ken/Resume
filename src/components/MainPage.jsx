9f4dbea97285634b1ed7dfa2a80a431777f11d82
akshay-ken <akshaybest3120@gmail.com>
2025-08-21 22:21:46 +0530

    feat: use usestate hook to manage toggle switch and useeffect to toggle dark class with darkMode as dependency array

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import { HeaderSection } from "./HeaderSection";
 
 export function MainPage() {
+  const [darkMode, setDarkMode] = useState(false);
+  useEffect(
+    function () {
+      // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+      if (darkMode) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+
+      console.log("working");
+    },
+    [darkMode]
+  );
+
+  function handleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
+
   return (
     <main>
-      <HeaderSection />
+      <HeaderSection toggleDarkMode={handleDarkMode} checkValue={darkMode} />
     </main>
   );
 }
