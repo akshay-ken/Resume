a05a71594ea2582f56e193f828df665e1ebfccda
akshay-ken <akshaybest3120@gmail.com>
2025-08-22 13:24:08 +0530

    style: change flex basis to fractions

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -5,16 +5,16 @@ import { IoLogoGithub } from "react-icons/io";
 import { FaLocationDot } from "react-icons/fa6";
 export function ContactUs() {
   return (
-    <nav className="flex flex-row md:flex-row border-t-4 border-zinc-900 dark:border-zinc-400">
-      <div className="basis-3xs md:basis-sm">
+    <nav className="flex flex-row flex-wrap border-t-4 border-zinc-900 dark:border-zinc-400">
+      <div className="basis-1/2 md:basis-sm bg-rose-400">
         <IoIosMail className="size-6 md:size-8" />
         <FaLinkedin className="size-6 md:size-8" />
       </div>
-      <div className="basis-3xs md:basis-sm">
+      <div className="basis-1/2 md:basis-sm bg-lime-400">
         <IoLogoWhatsapp className="size-6 md:size-8" />
         <IoLogoGithub className="size-6 md:size-8" />
       </div>
-      <div className="basis-sm md:basis-lg">
+      <div className="basis-1/2 md:basis-lg bg-red-500">
         <FaLocationDot className="size-6 md:size-8" />
       </div>
     </nav>
