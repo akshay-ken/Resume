ba907b91a6d65face0351d4641be7f91b935bd34
akshay-ken <akshaybest3120@gmail.com>
2025-08-22 18:05:00 +0530

    feat: import icons and set grid layout

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,4 +1,9 @@
 import { FaReact } from "react-icons/fa";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { PiFileJsx } from "react-icons/pi";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaGitAlt } from "react-icons/fa";
 export function FooterSection() {
   return (
     <footer>
@@ -9,9 +14,26 @@ export function FooterSection() {
       <div className="flex flex-row">
         <section className="w-[40vw] p-1 md:p-4 bg-sky-400 border-r-2 font-zcool border-dashed border-zinc-500 dark:border-zinc-50">
           <div className="grid grid-rows-3 grid-cols-4">
-            <div>
+            <div className="flex flex-row items-center justify-center col-span-2">
               <FaReact />
               React.js
+            </div>
+            <div className="flex flex-row items-center justify-center col-span-2">
+              <LuLayoutDashboard />
+              Grid & Flexbox
+            </div>
+            <div className="flex flex-row items-center justify-center col-span-3 border-r-2">
+              <RiTailwindCssFill />
+              Tailwind CSS
+            </div>
+            <div className="flex flex-row items-center justify-center">
+              <PiFileJsx />
+            </div>
+            <div className="flex flex-row items-center justify-center col-span-3">
+              <IoLogoJavascript /> JavaScript
+            </div>
+            <div className="flex flex-row items-center justify-center">
+              <FaGitAlt />
             </div>
           </div>
         </section>
