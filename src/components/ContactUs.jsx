e2c30424703750cfbf83ecef49b4dc94ee94ead3
akshay-ken <akshaybest3120@gmail.com>
2025-08-22 13:26:19 +0530

    style: add padding to container

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -5,7 +5,7 @@ import { IoLogoGithub } from "react-icons/io";
 import { FaLocationDot } from "react-icons/fa6";
 export function ContactUs() {
   return (
-    <nav className="flex flex-row flex-wrap border-t-4 border-zinc-900 dark:border-zinc-400">
+    <nav className="flex flex-row flex-wrap border-t-4 border-zinc-900 dark:border-zinc-400 p-1 md:p-4">
       <div className="basis-1/2 md:basis-sm bg-rose-400">
         <IoIosMail className="size-6 md:size-8" />
         <FaLinkedin className="size-6 md:size-8" />
@@ -14,7 +14,7 @@ export function ContactUs() {
         <IoLogoWhatsapp className="size-6 md:size-8" />
         <IoLogoGithub className="size-6 md:size-8" />
       </div>
-      <div className="basis-1/2 md:basis-lg bg-red-500">
+      <div className="basis-full md:basis-lg bg-red-500">
         <FaLocationDot className="size-6 md:size-8" />
       </div>
     </nav>
