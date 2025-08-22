4b2f225029846a8d2646aeed88a8a9e48e543758
akshay-ken <akshaybest3120@gmail.com>
2025-08-22 12:53:49 +0530

    feat: import logos from react icons

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,8 +1,18 @@
+import { IoIosMail } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { IoLogoGithub } from "react-icons/io";
 export function ContactUs() {
   return (
     <nav className="flex flex-col md:flex-row border-t-4 border-zinc-900 dark:border-zinc-400">
-      <div></div>
-      <div></div>
+      <div>
+        <IoIosMail />
+        <FaLinkedin />
+      </div>
+      <div>
+        <IoLogoWhatsapp />
+        <IoLogoGithub />
+      </div>
       <div></div>
     </nav>
   );
