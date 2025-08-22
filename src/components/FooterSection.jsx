ab3815ae3d3807c4b235001397ceffffb5049ddc
akshay-ken <akshaybest3120@gmail.com>
2025-08-22 19:15:53 +0530

    fix: trying to fix education section

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -12,7 +12,7 @@ export function FooterSection() {
         <p>Education</p>
       </div>
       <div className="flex flex-row mb-8">
-        <section className="w-[40vw] p-1 md:p-4 border-r-2 text-sm md:text-4xl dark:text-zinc-200 font-zcool border-dashed border-zinc-500 dark:border-zinc-50">
+        <section className="w-[40vw] p-1 md:p-4 border-r-2 text-sm md:text-4xl dark:text-zinc-900 font-zcool border-dashed border-zinc-500 dark:border-zinc-50">
           <div className="grid grid-rows-3 grid-cols-4 gap-1 md:gap-4">
             <div className="flex flex-row items-center justify-center col-span-2 gap-x-1 md:gap-x-4 bg-zinc-200 dark:bg-zinc-400 md:py-4 rounded-md">
               <FaReact />
@@ -37,7 +37,36 @@ export function FooterSection() {
             </div>
           </div>
         </section>
-        <section className="w-[60vw] p-1 md:p-4 bg-rose-400"></section>
+        <section className="w-[60vw] p-1 md:p-4 flex flex-col gap-y-1 md:gap-y-4">
+          <div className="flex flex-row items-center">
+            <p className="font-zcool md:text-xl text-xs text-red-400 dark:bg-zinc-600 p-1 md:p-2 rounded-lg bg-zinc-200">
+              College Dropout
+            </p>
+            <p className="font-web text-[8px] md:text-base leading-3 md:leading-6 tracking-tighter md:tracking-wide dark:bg-zinc-600 p-1 text-center md:p-2 rounded-lg bg-zinc-200">
+              Dropped out of engineering in 2018 due to financial issues.
+              Distance learning delayed by COVID; now focused on self-taught web
+              dev with plans for BCA alongside stable employment.
+            </p>
+          </div>
+          <div className="flex flex-row font-zcool">
+            <p className="text-xl md:text-5xl p-1 md:p-2 self-center basis-1/4 bg-sky-400">
+              12 <sup>th</sup>
+            </p>
+            <div className="flex flex-col basis-3/4 bg-amber-300 p-1 md:p-2">
+              <div className="flex flex-row justify-between ">
+                <p>HSC</p>
+                <p>2017</p>
+                <p>62%</p>
+              </div>
+              <div className="flex flex-row justify-between text-xs">
+                <p>C.K.T college, panvel</p>
+
+                <p>Computer Science</p>
+              </div>
+            </div>
+          </div>
+          <div></div>
+        </section>
       </div>
     </footer>
   );
