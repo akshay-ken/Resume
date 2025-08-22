58d7b90c06bfc9bf104b2e92e5691b76603d59a9
akshay-ken <akshaybest3120@gmail.com>
2025-08-22 14:51:39 +0530

    style: add text styling and dark mode wiht bg and variable text size

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -4,6 +4,18 @@ export function ProjectSection() {
       <div className="font-zcool text-xl md:text-3xl border-y-4 text-center bg-zinc-300 dark:bg-zinc-600 dark:text-zinc-300 border-zinc-900 dark:border-zinc-400">
         Projects
       </div>
+
+      <section className="flex flex-col p-1 md:p-4 gap-y-2 md:gap-y-5">
+        <div className="flex flex-row justify-between font-zcool">
+          <p className="text-base md:text-2xl bg-zinc-200 dark:bg-zinc-300 dark:text-zinc-700 p-1 md:p-2 rounded-sm tracking-tighter md:tracking-wide leading-4 md:leading-6">
+            Frontend Mentor Challenges {" (35+ Projects)"}
+          </p>
+          <p className="text-base md:text-2xl bg-zinc-200 dark:bg-zinc-300 dark:text-zinc-700 p-1 md:p-2 rounded-sm tracking-tighter md:tracking-wide leading-4 md:leading-6">
+            Technologies:-{" "}
+            <span className="text-xs md:text-base">React.js Tailwind CSS</span>
+          </p>
+        </div>
+      </section>
     </section>
   );
 }
