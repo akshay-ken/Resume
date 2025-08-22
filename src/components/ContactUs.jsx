7e2dcc20a8b38e578cd5f4303b874cefdc0aec7d
akshay-ken <akshaybest3120@gmail.com>
2025-08-22 13:37:18 +0530

    style: add text styles

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -5,9 +5,12 @@ import { IoLogoGithub } from "react-icons/io";
 import { FaLocationDot } from "react-icons/fa6";
 export function ContactUs() {
   return (
-    <nav className="flex flex-row flex-wrap border-t-4 border-zinc-900 dark:border-zinc-400 p-1 md:p-4">
+    <nav className="flex flex-row flex-wrap text-sm tracking-tighter md:text-xl md:tracking-wide dark:text-zinc-200 border-t-4 font-web border-zinc-900 dark:border-zinc-400 p-1 md:p-4">
       <div className="basis-1/2 md:basis-sm ">
-        <IoIosMail className="size-6 md:size-8 fill-zinc-900 dark:fill-zinc-400" />
+        <a href="" className="flex flex-row">
+          <IoIosMail className="size-6 md:size-8 fill-zinc-900 dark:fill-zinc-400" />
+          google
+        </a>
         <FaLinkedin className="size-6 md:size-8 fill-zinc-900 dark:fill-zinc-400" />
       </div>
       <div className="basis-1/2 md:basis-sm ">
