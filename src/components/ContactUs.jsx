558476f1bf8b8f681982697ce21908427d86c28a
akshay-ken <akshaybest3120@gmail.com>
2025-08-22 13:30:10 +0530

    style: fill color with dark var

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -6,16 +6,16 @@ import { FaLocationDot } from "react-icons/fa6";
 export function ContactUs() {
   return (
     <nav className="flex flex-row flex-wrap border-t-4 border-zinc-900 dark:border-zinc-400 p-1 md:p-4">
-      <div className="basis-1/2 md:basis-sm bg-rose-400">
-        <IoIosMail className="size-6 md:size-8" />
-        <FaLinkedin className="size-6 md:size-8" />
+      <div className="basis-1/2 md:basis-sm ">
+        <IoIosMail className="size-6 md:size-8 fill-zinc-900 dark:fill-zinc-400" />
+        <FaLinkedin className="size-6 md:size-8 fill-zinc-900 dark:fill-zinc-400" />
       </div>
-      <div className="basis-1/2 md:basis-sm bg-lime-400">
-        <IoLogoWhatsapp className="size-6 md:size-8" />
-        <IoLogoGithub className="size-6 md:size-8" />
+      <div className="basis-1/2 md:basis-sm ">
+        <IoLogoWhatsapp className="size-6 md:size-8 fill-zinc-900 dark:fill-zinc-400" />
+        <IoLogoGithub className="size-6 md:size-8 fill-zinc-900 dark:fill-zinc-400" />
       </div>
-      <div className="basis-full md:basis-lg bg-red-500">
-        <FaLocationDot className="size-6 md:size-8" />
+      <div className="basis-full md:basis-lg ">
+        <FaLocationDot className="size-6 md:size-8 fill-zinc-900 dark:fill-zinc-400" />
       </div>
     </nav>
   );
