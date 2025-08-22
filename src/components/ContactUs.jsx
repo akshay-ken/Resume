7494be2077cc13f9f796856af5d5f86320f14aa8
akshay-ken <akshaybest3120@gmail.com>
2025-08-22 13:16:50 +0530

    style: add basis for fxied dimensions

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -4,12 +4,12 @@ import { IoLogoWhatsapp } from "react-icons/io";
 import { IoLogoGithub } from "react-icons/io";
 export function ContactUs() {
   return (
-    <nav className="flex flex-col md:flex-row border-t-4 border-zinc-900 dark:border-zinc-400">
-      <div>
+    <nav className="flex flex-row md:flex-row border-t-4 border-zinc-900 dark:border-zinc-400">
+      <div className="basis-3xs md:basis-sm">
         <IoIosMail className="size-6 md:size-8" />
         <FaLinkedin className="size-6 md:size-8" />
       </div>
-      <div>
+      <div className="basis-3xs md:basis-sm">
         <IoLogoWhatsapp className="size-6 md:size-8" />
         <IoLogoGithub className="size-6 md:size-8" />
       </div>
