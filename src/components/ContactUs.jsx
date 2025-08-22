0b5f00504609ef7cff79bd4242cbddf69751960a
akshay-ken <akshaybest3120@gmail.com>
2025-08-22 12:47:57 +0530

    style: dark var for border color

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,6 +1,6 @@
 export function ContactUs() {
   return (
-    <nav className="flex flex-col md:flex-row border-t-4 border-zinc-900">
+    <nav className="flex flex-col md:flex-row border-t-4 border-zinc-900 dark:border-zinc-400">
       <div></div>
       <div></div>
       <div></div>
