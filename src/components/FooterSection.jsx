fdcc02cb629dc6675502cbc1f64166925729620d
akshay-ken <akshaybest3120@gmail.com>
2025-08-22 17:43:35 +0530

    style: add section divider with dark mode styling

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,6 +1,11 @@
 export function FooterSection() {
   return (
     <footer>
+      <div className="font-zcool text-xl md:text-3xl border-y-4 border-zinc-900 dark:border-zinc-400 flex flex-row justify-around bg-zinc-300 dark:bg-zinc-600 dark:text-zinc-300">
+        <p>Skills</p>
+        <p>Education</p>
+      </div>
+      <section></section>
       <section></section>
     </footer>
   );
