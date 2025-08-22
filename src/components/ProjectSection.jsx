ad69353c15feae374d612ae6343a131c62f17a54
akshay-ken <akshaybest3120@gmail.com>
2025-08-22 14:33:34 +0530

    style: add styling with dark mode

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,7 +1,9 @@
 export function ProjectSection() {
   return (
     <section>
-      <div></div>
+      <div className="font-zcool text-xl md:text-3xl border-y-4 text-center bg-zinc-300 dark:bg-zinc-600 dark:text-zinc-300 border-zinc-900 dark:border-zinc-400">
+        Projects
+      </div>
     </section>
   );
 }
